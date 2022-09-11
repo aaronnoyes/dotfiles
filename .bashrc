1e65a9a02a15d1949f4de2447f0afe7892dc9724
Aaron Noyes <aaron.noyes@me.com>
2022-09-11 14:48:26 -0500

    Minor tweaks

--- .bashrc
+++ .bashrc
@@ -8,3 +8,4 @@ eval "`fnm env`"
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
 export PS1="\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\W\[\033[00m\] \$ "
+
--- .config/nvim/lua/user/bufferline.lua
+++ .config/nvim/lua/user/bufferline.lua
@@ -1,2 +1,6 @@
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+require("bufferline").setup{
+	options = {
+		offsets = { { filetype = "NvimTree", text = "", padding = 0 } }
+	}
+}
