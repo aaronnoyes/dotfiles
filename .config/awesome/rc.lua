3788b1a47d8c234cc21c6a5138418d88f7489f9d
Aaron Noyes <aaron.noyes@me.com>
2022-09-15 00:32:04 -0500

    Factored out client, cleaned comments

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -2,49 +2,12 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
--- Standard awesome library
-local gears = require("gears")
-local awful = require("awful")
-require("awful.autofocus")
--- Widget and layout library
--- Theme handling library
-
-require("errors")
-
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
-
--- This is used later as the default terminal and editor to run.
 terminal = "kitty"
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
+require("awful.autofocus")
+require("errors")
 require("screen")
-
-
--- {{{ Key bindings
 require("keys")
-
-
-clientbuttons = gears.table.join(
-	awful.button({}, 1, function(c)
-		c:emit_signal("request::activate", "mouse_click", { raise = true })
-	end),
-	awful.button({ modkey }, 1, function(c)
-		c:emit_signal("request::activate", "mouse_click", { raise = true })
-		awful.mouse.client.move(c)
-	end),
-	awful.button({ modkey }, 3, function(c)
-		c:emit_signal("request::activate", "mouse_click", { raise = true })
-		awful.mouse.client.resize(c)
-	end)
-)
-
-
 require("rules")
 require("client")
