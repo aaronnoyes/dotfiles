c76158cd2d234cb171f03689d5a9fddac1f35925
Aaron Noyes <aaron.noyes@me.com>
2022-09-18 20:59:49 -0500

    Moved clientkeys and clientbuttons

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -26,20 +26,6 @@ default_layouts = {
 	--awful.layout.suit.corner.se,
 }
 
-clientbuttons = gears.table.join(
-    awful.button({ }, 1, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-    end),
-    awful.button({ modkey }, 1, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-        awful.mouse.client.move(c)
-    end),
-    awful.button({ modkey }, 3, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-        awful.mouse.client.resize(c)
-    end)
-)
-
 require("awful.autofocus")
 require("errors")
 require("screen")
