b766b333fa519ccc0c6f1ed300ef67b89bd0df87
Aaron Noyes <aaron.noyes@me.com>
2022-09-19 22:35:49 -0500

    Fixed duplicate promptbox

--- .config/awesome/screen.lua
+++ .config/awesome/screen.lua
@@ -170,7 +170,6 @@ awful.screen.connect_for_each_screen(function(s)
 			layout = wibox.layout.fixed.horizontal,
 			--mylauncher,
 			s.mytaglist,
-			s.mypromptbox,
 		},
 		{ -- Right widgets
 			layout = wibox.layout.fixed.horizontal,
