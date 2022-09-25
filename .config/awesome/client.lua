39db3d9aa3c4669e223a81818c4c58a15d9a8192
Aaron Noyes <aaron.noyes@me.com>
2022-09-24 21:11:57 -0500

    Added fallback icon for tasklist

--- .config/awesome/client.lua
+++ .config/awesome/client.lua
@@ -71,3 +71,15 @@ client.connect_signal("unfocus", function(c)
 end)
 -- }}}
 
+client.connect_signal("manage", function(c)
+    local cairo = require("lgi").cairo
+    local default_icon = gears.filesystem.get_dir("config") .. "fallback.png"
+    if c and c.valid and not c.icon then
+        local s = gears.surface(default_icon)
+        local img = cairo.ImageSurface.create(cairo.Format.ARGB32, s:get_width(), s:get_height())
+        local cr = cairo.Context(img)
+        cr:set_source_surface(s, 0, 0)
+        cr:paint()
+        c.icon = img._native
+    end
+end)
