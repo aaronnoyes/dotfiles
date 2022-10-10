3c33dc89834ec79af881a5891c8f2fb37fa29254
Aaron Noyes <aaron.noyes@me.com>
2022-10-10 00:09:55 -0500

    Added some clipbaord support for WSL

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -9,3 +9,12 @@ local options = {
 for k, v in pairs(options) do
 	vim.opt[k] = v
 end
+
+vim.cmd([[
+  if system('uname -r') =~ "microsoft"
+		  augroup Yank
+		  autocmd!
+		  autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
+		  augroup END
+  endif
+]])
