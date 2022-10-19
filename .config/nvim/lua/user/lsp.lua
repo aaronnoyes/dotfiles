64333c78b3025f9c9ed4c3290aa6f59cf1a610fd
Aaron Noyes <aaron.noyes@me.com>
2022-10-18 22:36:38 -0500

    Updated depricated function

--- .config/nvim/lua/user/lsp.lua
+++ .config/nvim/lua/user/lsp.lua
@@ -72,7 +72,7 @@ cmp.setup.cmdline(':', {
 	})
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require("mason-lspconfig").setup_handlers {
 		function (server_name)
