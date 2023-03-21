8b94452ede2be742305264d790036284afba3c36
Aaron Noyes <aaron.noyes@me.com>
2023-03-20 21:04:34 -0500

    Switch to LazyVim

--- .config/nvim/lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
--- require("no-clown-fiesta").setup({})
--- vim.cmd [[colorscheme no-clown-fiesta"]]
-
--- require('onedark').setup {
---     style = 'warmer'
--- }
--- require('onedark').load()
-
-vim.cmd [[colorscheme catppuccin-mocha"]]
