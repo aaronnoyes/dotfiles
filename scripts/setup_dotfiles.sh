7b00702ee3bbbd3e3cb658f64781171bbf2795f1
= <aaron@taiv.tv>
2024-03-09 17:26:34 -0600

    fixed typo

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -3,4 +3,4 @@ git clone --bare git@github.com:aaronnoyes/dotfiles.git ~/.dotfiles
 rm $HOME/.bashrc
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
-source $HOME/bashrc
+source $HOME/.bashrc
