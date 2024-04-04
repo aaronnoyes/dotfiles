c80cd3794a1747c39a4afeab5e443f574ddfaf8d
Aaron Noyes <aaron.noyes@me.com>
2024-04-04 01:32:00 -0500

    fixed lsp and added neodev

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,6 +22,7 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+
 require('lazy').setup({
 	{ 
 		"miikanissi/modus-themes.nvim", 
@@ -119,25 +120,55 @@ require('lazy').setup({
 		end
 	},
 	{
-		"neovim/nvim-lspconfig"
-	},
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
 		dependencies = {
-			"williamboman/mason.nvim"
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			{ "folke/neodev.nvim", opts = {} },
+			'WhoIsSethDaniel/mason-tool-installer.nvim'
 		},
 		config = function()
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+			local servers = {
+				clangd = {},
+				-- rust_analyzer = {},
+				tsserver = {},
+
+				lua_ls = {
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = 'Replace',
+							},
+							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+							-- diagnostics = { disable = { 'missing-fields' } },
+						},
+					},
+				},
+			}
+
 			require("mason").setup()
-			require("mason-lspconfig").setup()
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup({})
-				end
+
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, {
+				'stylua',
 			})
+			require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+			require('mason-lspconfig').setup {
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						-- This handles overriding only values explicitly passed
+						-- by the server configuration above. Useful when disabling
+						-- certain features of an LSP (for example, turning off formatting for tsserver)
+						server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+						require('lspconfig')[server_name].setup(server)
+					end,
+				},
+			}
 		end
 	},
 })
