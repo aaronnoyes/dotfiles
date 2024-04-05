b113f2a01cbaa88f4aaac8b64faf5bfba766ecbc
Aaron Noyes <aaron.noyes@me.com>
2024-04-04 22:36:26 -0500

    mini surround and auto pairs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -283,5 +283,31 @@ require('lazy').setup({
 						end,
 					},
 				},
-			}
+			},
+			{
+				'echasnovski/mini.pairs',
+				version = '*',
+				config = function ()
+					require("mini.pairs").setup()
+				end
+			},
+			{
+				'echasnovski/mini.surround',
+				version = '*',
+				config = function ()
+					require("mini.surround").setup()
+				end,
+				mappings = {
+					add = 'sa', -- Add surrounding in Normal and Visual modes
+					delete = 'sd', -- Delete surrounding
+					find = 'sf', -- Find surrounding (to the right)
+					find_left = 'sF', -- Find surrounding (to the left)
+					highlight = 'sh', -- Highlight surrounding
+					replace = 'sr', -- Replace surrounding
+					update_n_lines = 'sn', -- Update `n_lines`
+
+					suffix_last = 'l', -- Suffix to search with "prev" method
+					suffix_next = 'n', -- Suffix to search with "next" method
+				},
+			},
 		})
