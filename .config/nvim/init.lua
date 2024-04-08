dcd29d23fb932ab3ea04511d89820dbc4a482ff9
= <aaron@taiv.tv>
2024-04-08 11:29:51 -0500

    added git signs and neotree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -340,4 +340,33 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		keys = {
+			{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "files tree" },
+		},
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				filesystem = {
+					filtered_items = {
+						hide_gitignored = true,
+					},
+				},
+			})
+		end,
+	},
 })
