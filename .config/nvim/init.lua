3fc65e3a1339cb2b2645cc4d9526f2d580f53949
aaron-taiv <aaron@taiv.tv>
2024-06-14 12:14:55 -0500

    fixed doubled quotes in prettier

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -342,23 +342,15 @@ require("lazy").setup({
           lua = { "stylua" },
           -- Conform will run multiple formatters sequentially
           python = { "isort", "black" },
-          -- Use a sub-list to run only the first available formatter
           typescript = { { "prettierd", "prettier" } },
           typescriptreact = { { "prettierd", "prettier" } },
           javascript = { { "prettierd", "prettier" } },
           javascriptreact = { { "prettierd", "prettier" } },
         },
         format_on_save = {
-          -- These options will be passed to conform.format()
           timeout_ms = 500,
           lsp_fallback = false,
         },
-      })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
-        callback = function(args)
-          require("conform").format({ bufnr = args.buf })
-        end,
       })
     end,
   },
