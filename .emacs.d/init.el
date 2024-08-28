d28cb89ebf60de9026942731044950cb9567ccaa
aaron-taiv <aaron@taiv.tv>
2024-08-28 17:48:33 -0500

    fresh emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,58 @@
+(tool-bar-mode 0)
+(scroll-bar-mode -1)
+(global-display-line-numbers-mode 1)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4c7228157ba3a48c288ad8ef83c490b94cb29ef01236205e360c2c4db200bb18" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" default))
+ '(package-selected-packages '(vertico orderless consult nord-theme gruvbox-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+
+(use-package nord-theme
+  :ensure t
+  :init
+  (load-theme 'nord t))
+
+;; Vertico + Consult + Orderless + Embark + Marginalia + Corfu.
+;; Enable vertico
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :ensure t
+  :init
+  (savehist-mode))
+
+(use-package consult
+  :ensure t
+  )
+
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
