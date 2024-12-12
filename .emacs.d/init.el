0496e422113d02021b496a1b9bcd3a3b28de8436
Aaron Noyes <aaron.noyes@me.com>
2024-12-12 09:35:42 -0600

    new year new me

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,20 +1,6 @@
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
 (menu-bar-mode -1)
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
-(setq ns-use-proxy-icon nil)
-(setq frame-title-format nil)
-; defaults write org.gnu.Emacs HideDocumentIcon YES
-
-(global-display-line-numbers-mode 1)
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
-(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -22,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(corfu marginalia consult vertico cape orderless magit company counsel ivy treesit-auto tree-sitter flycheck lsp-mode projectile ripgrep nord-theme)))
+   '(which-key vertico spacemacs-theme orderless marginalia lua-mode corfu cape)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -30,23 +16,19 @@
  ;; If there is more than one, they won't work right.
  )
 
-(use-package nord-theme
-  :ensure t
-  :init
-  (load-theme 'nord t))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
-(use-package ripgrep
-  :ensure t)
-
-(use-package projectile
+(use-package spacemacs-theme
   :ensure t
-  :after (ripgrep)
-  :init
-  (projectile-mode))
+  :config
+  (load-theme 'spacemacs-dark :noconfimr))
 
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
+;; tree-sitter
+;; https://git.savannah.gnu.org/cgit/emacs.git/tree/admin/notes/tree-sitter/starter-guide?h=emacs-29
+;; https://github.com/casouri/tree-sitter-module/releases
+;; grammars go in ~/.emacs.d/tree-sitter
 
 (use-package vertico
   :ensure t
@@ -67,29 +49,31 @@
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion)))))
 
-(use-package consult
-  :ensure t)
-
 (use-package marginalia
   :ensure t
-  :bind (:map minibuffer-local-map
-         ("M-A" . marginalia-cycle))
   :init
   (marginalia-mode))
 
 (use-package corfu
   :ensure t
   :custom
-  (corfu-cycle t)
   (corfu-auto t)
+  :init
+  (global-corfu-mode)
+  (setq corfu-popupinfo-delay 0.2)
+  (corfu-popupinfo-mode))
+
+;; Enable Corfu completion UI
+;; See the Corfu README for more configuration tips.
+(use-package corfu
   :init
   (global-corfu-mode))
 
+;; Add extensions
 (use-package cape
-  :ensure t
   ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
   ;; Press C-c p ? to for help.
-  :bind ("C-c p" . cape-prefix-map) ;; Alternative keys: M-p, M-+, ...
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative key: M-<tab>, M-p, M-+
   ;; Alternatively bind Cape commands individually.
   ;; :bind (("C-c p d" . cape-dabbrev)
   ;;        ("C-c p h" . cape-history)
@@ -100,6 +84,14 @@
   ;; used by `completion-at-point'.  The order of the functions matters, the
   ;; first function returning a result wins.  Note that the list of buffer-local
   ;; completion functions takes precedence over the global list.
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-  (add-to-list 'completion-at-point-functions #'cape-file)
-  (add-to-list 'completion-at-point-functions #'cape-elisp-block))
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+)
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
