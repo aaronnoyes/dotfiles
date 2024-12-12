262a5461963afdc981f8243f902afefe88261ab9
Aaron Noyes <aaron.noyes@me.com>
2024-12-12 09:49:47 -0600

    vterm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(which-key vertico spacemacs-theme orderless marginalia lua-mode corfu cape)))
+   '(vterm which-key vertico spacemacs-theme orderless marginalia lua-mode corfu cape)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -95,3 +95,14 @@
   :ensure t
   :init
   (which-key-mode))
+
+(use-package vterm
+  :ensure t
+  :config
+  (defun my/vterm-new ()
+    "Create a new vterm buffer with a unique name."
+    (interactive)
+    (let ((buffer (generate-new-buffer-name "vterm")))
+      (vterm buffer)))
+  :bind
+  (("C-c t" . my/vterm-new)))
